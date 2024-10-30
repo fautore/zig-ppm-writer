db2136adec17103adde096b402cc7bace4b7d6d8
fautore <fautore@protonmail.com>
2024-10-31 00:39:13 +0100

    write arbitrary buffer

--- src/main.zig
+++ src/main.zig
@@ -3,20 +3,43 @@ const std = @import("std");
 const WIDTH = 600;
 const HEIGHT = 600;
 
+const Vec3 = struct {
+    x: u8,
+    y: u8,
+    z: u8,
+};
+
 pub fn main() !void {
-    const file = try std.fs.cwd().createFile(
+    var file = try std.fs.cwd().createFile(
         "test.ppm",
         .{ .read = true },
     );
     defer file.close();
-    const str = try std.fmt.allocPrint(std.heap.page_allocator, "P6\n{} {}\n255\n", .{ WIDTH, HEIGHT });
-    _ = try file.write(str);
-    const p1 = [_]u8{
-        255,
-        0,
-        255,
+
+    var pixels = [_]Vec3{
+        Vec3{ .x = 255, .y = 0, .z = 255 },
+        Vec3{ .x = 0, .y = 0, .z = 0 },
+        Vec3{ .x = 255, .y = 0, .z = 255 },
+        Vec3{ .x = 0, .y = 0, .z = 0 },
     };
-    _ = try file.write(p1[0..p1.len]);
+
+    var pixSlice: []Vec3 = pixels[0..];
+    try renderPPMBuffer(&pixSlice, &file);
+}
+
+fn renderPPMBuffer(pixels: *[]Vec3, file: *std.fs.File) !void {
+    const pix = pixels.*;
+    const header = try std.fmt.allocPrint(std.heap.page_allocator, "P6\n{} {}\n255\n", .{ WIDTH, HEIGHT });
+    _ = try file.write(header);
+    var buf = std.io.bufferedWriter(file.writer());
+    var w = buf.writer();
+    for (pix, 0..) |p, i| {
+        if (i * 3 % buf.buf.len == 0) {
+            try buf.flush();
+        }
+        const b = [_]u8{ p.x, p.y, p.z };
+        _ = try w.write(&b);
+    }
 }
 
 test "simple test" {}
